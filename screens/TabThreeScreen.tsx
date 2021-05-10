109b05ad9c32a6eed3d3cbfaaacecc1a7c7f95b7
UrhSmrkolj <urh.smrkolj@gmail.com>
2021-05-10 15:56:40 +0200

    device ui

--- screens/TabThreeScreen.tsx
+++ screens/TabThreeScreen.tsx
@@ -1,13 +1,29 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
-
 import { Text, View } from '../components/Themed';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+
+const getBackgroundColor = (value) => {
+  let color;
+  if (value === 0) {
+      color = 'green';
+  } else {
+      color = 'red';
+  }
+  return color;
+};
+
 
 export default function TabThreeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Notin</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <View style={[styles.bar, {width: 200, height: 50, alignItems: 'center', justifyContent: 'center', backgroundColor: getBackgroundColor(0)}]}>
+        <View>
+          <FontAwesome5 name={'link'} size={20} color={'white'}/>
+        </View>
+        
+
+      </View>
     </View>
   );
 }
@@ -21,6 +37,10 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+  },
+  bar: {
+    height: 20,
+    width: 20
   },
   separator: {
     marginVertical: 30,
