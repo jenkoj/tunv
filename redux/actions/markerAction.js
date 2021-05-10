ed4ee55d47ee375f56148bb83cea1d55794b13b0
Jakob <jakob.jenko@gmail.com>
2021-05-10 18:49:16 +0200

    rm redux dirs

--- redux/actions/markerAction.js
+++
@@ -1,9 +0,0 @@
-const setMarkerLocationAction = location => ({
-    type: 'SET_LOCATION',
-    location,
-});
-const updateMarkerLocationAction = () => ({
-    type: 'UPDATE_LOCATION',
-});
-
-export {setMarkerLocationAction, updateMarkerLocationAction};
