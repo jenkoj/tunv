adda52bb361d7c2dbba48db78440692ece5dfb13
Jakob <jakob.jenko@gmail.com>
2021-05-11 13:07:35 +0200

    new marker

--- screens/mapsScreen.tsx
+++ screens/mapsScreen.tsx
@@ -11,154 +11,152 @@ import Geolocation from '@react-native-community/geolocation';
 
 export default class App extends React.Component {
 
-constructor(props: {} | Readonly<{}>) {
-  super(props)
-
-  this.state = {
-    location: [,],
-    latitude: 46.11956480,
-    longitude: 14.83770500,
-    error: null,
-    marker: null
-    
+  constructor(props: {} | Readonly<{}>) {
+    super(props)
+  
+    this.state = {
+      location: [,],
+      latitude: 46.11956480,
+      longitude: 14.83770500,
+      error: null,
+      marker: null,
+      latitudeMarker: 0,
+      longitudeMarker: 0,   
+    }
   }
-}
-
-componentDidMount(){
-    console.log("fetching...")
-    getData().then((data: any)=> {
+  
+  componentDidMount(){
+      console.log("fetching...")
+      getData().then((data: any)=> {
+      
+       this.state.latitudeMarker = data.latitude;
+       this.state.longitudeMarker = data.longitude;
+  
+      this.setState({
+                latitude: data.latitude,
+                longitude: data.longitude,
+                error: null
+              })
+  
+        console.log("longitude: ",data.longitude)
+        console.log("latituude: ",data.latitude)
+        console.log("...fin")
+        
+  
     
-    // this.state.longitude = result.longitude;
-    // this.state.latitude = result.longitude;
-
-    this.setState({
-              latitude: data.latitude,
-              longitude: data.longitude,
-              error: null
-            })
-
-      console.log("longitude: ",data.longitude)
-      console.log("latituude: ", data.latitude)
-      console.log("...fin")
-            
+      }).catch((err: any) => {
+          console.log(err)
+    
+      });
+      //console.log(data)
+      
+      Geolocation.getCurrentPosition(position =>{
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null
+        });
+      },
+      error => this.setState({error: error.message}),
+      { enableHighAccuracy: true, timeout:2000, maximumAge:2000}
+      );
+    }
+  
+  
+  setMarker = (location: any)  => {
+    //dispatch(setMarkerLocationAction(location));
+    console.log("data to be stored: ")
+    console.log(location);
+    console.log("stroing!........");
+    storeData(location);  
+    console.log("........stored!");
+    
+    console.log("data stored: ")
+    
+    getData().then((result: any)=> {
+      console.log("longitude: ",result.longitude)
+      console.log("latitude: ", result.latitude)
+  
     }).catch((err: any) => {
         console.log(err)
   
     });
-    
-    Geolocation.getCurrentPosition(position =>{
-      this.setState({
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude,
-        error: null
-      });
-    },
-    error => this.setState({error: error.message}),
-    { enableHighAccuracy: true, timeout:2000, maximumAge:2000}
+  }
+  
+  
+  render(){
+    //const {setMarker} = this.props;
+    const {marker} = this.state;
+    return (
+  
+      <MapView
+      showsCompass
+      showsUserLocation 
+      loadingEnabled 
+      key={this.state.forceRefresh} 
+      style={styles.map} 
+      region={{
+        latitude: this.state.latitude,
+        longitude: this.state.longitude,
+        latitudeDelta: 0.01, 
+        longitudeDelta: 0.01,
+      }}
+  
+      onLongPress={(e) => this.setState({ marker: e.nativeEvent.coordinate, location: e.nativeEvent.coordinate})}
+      //onMarkerPress={e => { console.log(e.nativeEvent.coordinate); }}
+      onMarkerPress={() => this.setMarker(marker)}
+      
+      >
+  
+  
+      <Marker 
+        coordinate={{ latitude : this.state.latitudeMarker , longitude : this.state.longitudeMarker }} 
+        pinColor={'red'}
+        title={'BIKE LOCATION'}
+      />  
+  
+  
+      {
+            this.state.marker &&
+            <Marker 
+              coordinate={this.state.marker}
+              title={'SAVED!'}  
+            />
+      }
+  
+  
+      </MapView>
+      
     );
   }
-
-
-setMarker = (location: any)  => {
-  //dispatch(setMarkerLocationAction(location));
-  console.log("data to be stored: ")
-  console.log(location);
-  console.log("stroing!........");
-  storeData(location);  
-  console.log("........stored!");
-  
-  console.log("data stored: ")
-  
-  getData().then((result: any)=> {
-    console.log("longitude: ",result.longitude)
-    console.log("latitude: ", result.latitude)
-
-  }).catch((err: any) => {
-      console.log(err)
-
-  });
-}
-
-
-render(){
-  //const {setMarker} = this.props;
-  const {marker} = this.state;
-  return (
-
-    <MapView
-    showsCompass
-    showsUserLocation 
-    loadingEnabled 
-    key={this.state.forceRefresh} 
-    style={styles.map} 
-    region={{
-      latitude: this.state.latitude,
-      longitude: this.state.longitude,
-      latitudeDelta: 0.01, 
-      longitudeDelta: 0.01,
-    }}
-
-    onLongPress={(e) => this.setState({ marker: e.nativeEvent.coordinate, location: e.nativeEvent.coordinate})}
-    //onMarkerPress={e => { console.log(e.nativeEvent.coordinate); }}
-    onMarkerPress={() => this.setMarker(marker)}
-    
-    >
-
-     <Marker coordinate={{latitude: 46.142, longitude: 14.9689}} onPress={()=>console.log({location})}>
-      
-     </Marker>
-
-
-    {
-          this.state.marker &&
-          <Marker 
-            coordinate={this.state.marker}
-            title={'Bike'}  
-          />
-    }
-
-
-    </MapView>
-    
-  );
-}
-}
-
-
-/* 
-    <Marker
-        pinColor = '#0000FF' 
-        fixed
-        coordinate={this.state}
-        title={'You are here!'}
-    />
-*/
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  container1: {
-    flex: 1,
-  },
-  scrollView: {
-    flex: 1,
-    backgroundColor: 'pink',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-  map: {
-    height: '100%'
   }
-});
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    container1: {
+      flex: 1,
+    },
+    scrollView: {
+      flex: 1,
+      backgroundColor: 'pink',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    title: {
+      fontSize: 20,
+      fontWeight: 'bold',
+    },
+    separator: {
+      marginVertical: 30,
+      height: 1,
+      width: '80%',
+    },
+    map: {
+      height: '100%'
+    }
+  });
+  
