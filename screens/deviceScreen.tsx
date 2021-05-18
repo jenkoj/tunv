647b24cf1c0891b8f4491fc2e35d96d22c44a398
Jakob <jakob.jenko@gmail.com>
2021-05-18 13:03:13 +0200

    popup and scrollview

--- screens/deviceScreen.tsx
+++ screens/deviceScreen.tsx
@@ -1,8 +1,10 @@
 import React,{useCallback, useEffect, useMemo, useState, useRef} from 'react';
-import { Pressable, StyleSheet } from 'react-native';
+import { Pressable, StyleSheet, Image, Button, Modal } from 'react-native';
 import { Text, View } from '../components/Themed';
 import { Foundation } from '@expo/vector-icons'; 
 import { useColorScheme } from 'react-native-appearance';
+import { ScrollView } from 'react-native-gesture-handler';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import {storeData,getData} from "../storage/storageHandler"
 import {BleManager, Device, State} from 'react-native-ble-plx';
 import scanner from '../components/ble/scanner';
@@ -37,7 +39,8 @@ const deviceScreen = () => {
   const [timerState,setTrigerTimer] = useState<boolean>(false);
   //ids and con
   const [selID, setSelID] = useState<string>("0000180f-0000-1000-8000-00805f9b34fb");
-
+  //popup
+  const [popUp, setpopUpState] = useState(false);
   //loc
   const [userPos, setUserPos] = useState({latitude: null, longitude: null})
 
@@ -124,9 +127,11 @@ useEffect(()=>{
       if (data == "locked"){
         setButtonStyle('green')
         setToggleLock(true)
+        setLockState("locked")
       }else{
         setButtonStyle('red')
         setToggleLock(false)
+        setLockState("unlocked")
       }
 
     }).catch(()=>{})
@@ -257,7 +262,7 @@ const toggleLock= useCallback(() => {
 }, [locked]);
 
 let colorScheme = useColorScheme();
-
+/*
   return (
     <View style={styles.container}>
       
@@ -279,30 +284,160 @@ let colorScheme = useColorScheme();
   </View>
   )
 }
+*/
+return (
+  <SafeAreaView style={styles.container}>
+    <View style={styles.myDevices}>
+      <Image
+        //resizeMode='contain'
+        style={styles.imgConn}
+        source={{
+          uri: 'https://i.pinimg.com/736x/e5/4a/07/e54a071e4abe3340ddfb2222712fac51.jpg'
+        }}
+      />
+      <Text style={{ fontSize: 20, fontWeight: "bold", marginTop: 20 }}>My devices</Text>
+    </View>
+    <ScrollView style={styles.scrollView} fadingEdgeLength={0}>
+      <View style={[styles.button, { backgroundColor: buttonStyle }]}>
+        <Pressable
+          style={styles.button}
+          onPress={toggleLock}
+          onLongPress={() => { setpopUpState(true) }}
+        >
+          <Modal
+            transparent={true}
+            visible={popUp}
+          >
+            <View style={{ backgroundColor: "#000000aa", flex: 1 }}>
+              <View style={{ backgroundColor: "#FFFFFF", marginTop: 300, marginBottom: 300, marginLeft: 20, marginRight: 20, padding: 40, borderRadius: 10, flex: 1 }}>
+                <View style={{ flexDirection: "column", backgroundColor: "#FFFFFF", justifyContent: 'center', alignItems: 'center' }}>
+                  <View style={{ backgroundColor: "#FFFFFF", flexDirection: "row", marginBottom: 20 }}>
+                  <Text style={{ fontSize: 30, color: "#000000", marginLeft: 10 }}>lock status</Text>
+                  </View>
+                  
+                  <View style={{ backgroundColor: "#FFFFFF", flexDirection: "row" }}>
+
+                    <View style={{ backgroundColor: "#FFFFFF" }}>
+                      <Foundation name={"battery-half"} size={24} color="black" />
+                    </View>
+                    <View style={{ backgroundColor: "#FFFFFF" }}>
+                      <Text style={{ fontSize: 20, color: "#000000", marginLeft: 10 }}>  56%</Text>
+                    </View>
+                  </View>
+                  <View style={{ backgroundColor: "#FFFFFF", flexDirection: "row" }}>
+                    <View style={{ backgroundColor: "#FFFFFF" }}>
+                      <Foundation name={"compass"} size={24} color="black" />
+                    </View>
+                    <View style={{ backgroundColor: "#FFFFFF" }}>
+                      <Text style={{ fontSize: 20, color: "#000000", marginLeft: 10 }}>350 m</Text>
+                    </View>
+                  </View>
+                  <View style={{ marginTop: 40, backgroundColor: "#FFFFFF", height: 40, width: 70, justifyContent: 'center', alignItems: 'center' }}>
+                    <Pressable
+                      onPress={() => { setpopUpState(false) }}
+                    >
+                      <Foundation name={"x"} size={30} color="red" />
+                    </Pressable>
+
+                  </View>
+                </View>
+              </View>
+            </View>
+          </Modal>
+
+
+          <View style={[styles.text, { backgroundColor: buttonStyle }]}>
+            <Text>
+              Bike is {lockState}
+                          </Text>
+          </View>
+          <View style={[styles.icon, { backgroundColor: buttonStyle }]}>
+            <Foundation name={iconStyle} size={24} color="white" />
+          </View>
+        </Pressable>
+      </View>
+      <View style={[styles.button, { backgroundColor: 'red' }]}>
+        <Pressable
+          style={styles.button}
+        //onPress={toggleLock}
+        >
+          <View style={[styles.text, { backgroundColor: 'red' }]}>
+            <Text>
+              MTB is unlocked
+                          </Text>
+          </View>
+          <View style={[styles.icon, { backgroundColor: 'red' }]}>
+            <Foundation name={'link'} size={24} color="white" />
+          </View>
+        </Pressable>
+      </View>
+      <View style={[styles.button, { backgroundColor: 'red' }]}>
+        <Pressable
+          style={styles.button}
+        //onPress={toggleLock}
+        >
+          <View style={[styles.text, { backgroundColor: 'red' }]}>
+            <Text>
+              Road bike is unlocked
+                          </Text>
+          </View>
+          <View style={[styles.icon, { backgroundColor: 'red' }]}>
+            <Foundation name={'link'} size={24} color="white" />
+          </View>
+        </Pressable>
+      </View>
+      
+    </ScrollView>
+  </SafeAreaView>
+)
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingHorizontal: 10,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  icon: {
-  
-  },
-  button: {
-    borderRadius: 3,
-    alignItems: 'center', 
-    justifyContent: 'center', 
-    flexDirection: "row",
-    alignContent: "space-between",
-    height: 50,
-    width: 200,
-  },
-  text: {
-    flex: 0.8,
-    fontSize:20,
-  }
+container: {
+  flex: 1,
+  paddingHorizontal: 10,
+  alignItems: 'center',
+  justifyContent: 'center',
+},
+icon: {
+
+},
+button: {
+  borderRadius: 10,
+  alignItems: 'center',
+  justifyContent: 'center',
+  flexDirection: "row",
+  alignContent: "space-between",
+  height: 70,
+  width: 300,
+  margin: 10,
+},
+text: {
+  flex: 0.8,
+  fontSize: 20,
+},
+scrollView: {
+  //backgroundColor: 'pink',
+  //marginHorizontal: 0,
+  marginTop: 10,
+  marginBottom: 70,
+},
+myDevices: {
+  marginTop: 10,
+  alignItems: 'center',
+  justifyContent: 'center',
+},
+text1: {
+  fontSize: 20,
+},
+imgConn: {
+  marginBottom: 15,
+  //maxHeight: 'height',
+  //maxWidth: 'width',
+  height: 135,
+  width: 325,
+  borderRadius: 10,
+}
 });
 
 
